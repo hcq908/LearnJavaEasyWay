74eb0288a30b9522902fd143e0a4b15e710e6543
hcq908 <hcq908@163.com>
2018-09-25 20:18:39 +0800

    First commit

---
+++ src/Hilo.java
@@ -0,0 +1,7 @@
+public class Hilo {
+    public static void main(String[] args){
+//TODO Auto-generated method stub
+        int theNumber = (int)(Math.random()*100+1);
+
+    }
+}
---
+++ src/StartGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class StartGUI {
+    public static void main(String[] args){
+        JFrame myFrame = new JFrame("Hello"); //欢迎词
+        myFrame.setSize(300,200);
+        myFrame.setVisible(true);
+
+    }
+}
